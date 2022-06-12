8ddc352bb1e1546b0cad28cc214c8138013b33d9
Zhaohan Sun <szh6615nku@gmail.com>
2022-06-12 18:20:54 -0400

    Add data_check component

--- main.py
+++ main.py
@@ -66,10 +66,18 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+
+            _ = mlflow.run(
+                os.path.join(root_path, "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "sample.csv:latest",
+                    "ref": "sample.csv:reference",
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
